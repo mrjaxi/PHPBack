1d16b3d7ed4a5c871a1bb27ca481cea04c8cd057
BumbleBeeLion <bumblebeelion@atma.company>
2022-03-14 15:03:06 +0500

    rus lang

---
+++ application/language/rus/log_lang.php
@@ -0,0 +1,18 @@
+<?php
+    $lang['log_commented'] = 'Идея %s прокомментирована';
+    $lang['log_comment_deleted'] = 'Комментарий удален %s';
+    $lang['log_idea_approved'] = 'Идея одобрена %s';
+    $lang['log_idea_deleted'] = 'Идея отклонена %s';
+    $lang['log_idea_status'] = 'Статус идеи изменён %s1 to %s2';
+    $lang['log_user_registered'] = 'Пользователь зарегистрирован';
+    $lang['log_new_idea'] = 'Новая идея создана';
+    $lang['log_idea_voted'] = 'Идея %s1 за которую %s2 проголосовали';
+    $lang['log_user_banned'] = 'Пользователь %s1 заблокирован на %s2 дней';
+    $lang['log_user_was_banned'] = 'Пользователь %s был заблокирован';
+    $lang['log_user_unbanned'] = 'Пользователь %s был разблокирован';
+    $lang['log_settings'] = 'Настройки были изменены';
+    $lang['log_user_admin'] = 'Пользователь был обновлён администратором';
+    $lang['log_category_created'] = 'Категория создана';
+    $lang['log_category_description'] = 'описание обновлено';
+    $lang['log_category_changed'] = 'Категория \'%s1\' изменена на \'%s2\'';
+    $lang['log_category_deleted'] = 'Категория %s удалена';
