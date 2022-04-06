a4aef9060ffab53c683ec67d17b4523ef81079d5
Sergey <jaxi@atma.company>
2022-04-06 14:19:18 +0500

    Поправил немного стили в профиле пользователя

--- application/views/home/user.php
+++ application/views/home/user.php
@@ -8,7 +8,7 @@
             <li><?php echo $lang['label_profiles']; ?></li>
             <li class="active"><?php echo $user->name; ?></li>
         </ol></small>
-    <h5><?php echo $user->name; ?><?php if($user->isadmin): ?><span class="label label-danger" style="margin-left:7px;">Админ</span> <?php endif; ?></h5>
+    <h5><?php echo $user->name; ?><?php if($user->isadmin): ?><span class="label label-danger" style="margin-left:7px; border-radius: 35px">Админ</span> <?php endif; ?></h5>
     <?php if(isset($_SESSION['phpback_userid']) && $user->id == $_SESSION['phpback_userid']):?>
     <?php elseif(isset($_SESSION['phpback_isadmin']) && $_SESSION['phpback_isadmin'] > 1): ?>
         <a href="<?php echo base_url() . 'admin/users/' . $user->id; ?>" target="_blank"><button type="submit" class="btn btn-danger btn-sm" style="width:130px"><?php echo $lang['label_ban_user']; ?></button></a>
@@ -44,7 +44,7 @@
                         <tr>
                             <td><a href="<?= $vote['idea']->url; ?>"><?= $vote['idea']->title; ?></a></td>
                             <td><?= $vote['number']; ?></td>
-                            <td><a href="<?= base_url() . 'action/unvote/' . $vote['id'];?>"><button type="submit" class="btn btn-warning btn-sm" style="width:130px"><?php echo $lang['label_delete_votes']; ?></button></a></td>
+                            <td><a href="<?= base_url() . 'action/unvote/' . $vote['id'];?>"><button type="submit" class="btn btn-warning btn-sm" style="width:130px; border-radius: 35px"><?php echo $lang['label_delete_votes']; ?></button></a></td>
                         </tr>
                     <?php endforeach; ?>
                     </tbody>
